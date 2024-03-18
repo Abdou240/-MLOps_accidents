d8e59bf5d18be5cb4083c527f61dbe9044ede677
danielggm <daniel.ortegavz@gmail.com>
2024-03-18 21:55:53 +0000

    remove tests from API

--- docker-compose/API/test_main.py
+++ docker-compose/API/test_main.py
@@ -1,35 +1,35 @@
-from fastapi.testclient import TestClient
-from .main import app
-import pytest
+# from fastapi.testclient import TestClient
+# from .main import app
+# import pytest
 
-client = TestClient(app)
+# client = TestClient(app)
 
-# For all: test response for correct and incorrect data
+# # For all: test response for correct and incorrect data
 
-class TestAdmin:
-	def test_retrain_model():
-		assert client.post('/admin/model/retrain').status_code == 200
-	
-	def test_stats_model():
+# class TestAdmin:
+#     def test_retrain_model():
+#         assert client.post('/admin/model/retrain').status_code == 200
+    
+#     def test_stats_model():
 
-	def test_predict_model():
+#     def test_predict_model():
 
-	def test_users_list():
+#     def test_users_list():
 
-	def test_users_add():
+#     def test_users_add():
 
-	def test_users_remove():
+#     def test_users_remove():
 
-	def test_users_update():
+#     def test_users_update():
 
-class TestSuperUSer:
-	def test_gen_stats():
+# class TestSuperUSer:
+#     def test_gen_stats():
 
-	def test_stats_query():
+#     def test_stats_query():
 
-class TestUser:
-	def test_risky_locations():
-		tests = []
-		tests.append(client.get('/gen_user/risky_locations'))
+# class TestUser:
+#     def test_risky_locations():
+#         tests = []
+#         tests.append(client.get('/gen_user/risky_locations'))
 
-	def test_query_location():
+#     def test_query_location():
